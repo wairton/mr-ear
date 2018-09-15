4e69b888ca7bff8ca5b668f929a6b75c7986bc06
Wairton de Abreu <wairtonjr@gmail.com>
2018-09-15 14:35:31 +0200

    basic scale exercise WIP

--- exercise.py
+++ exercise.py
@@ -1,4 +1,5 @@
 import random
+from music import Note, notes, Reference, Semitones
 
 
 def get_random_note(lower=None, higher=None):
@@ -6,3 +7,15 @@ def get_random_note(lower=None, higher=None):
     higher = higher or Note('C', 8)
     n = random.randint(0, higher - lower)
     Player().perform([lower + Semitones(n)])
+
+
+def build_ascending_scale_exercise(min_root=notes.A2, max_root=notes.GS5, intervals=[Semitones(1)]):
+   min_reference = Reference.note_to_reference(min_root)
+   max_reference = Reference.note_to_reference(max_root)
+   exercises = []
+   for i in range(10):
+       root_note = Reference.reference_to_note(random.randint(min_reference, max_reference))
+       interval = random.choice(intervals)
+       other_note = root_note + interval
+       exercises.append((root_note, other_note, interval))
+   return exercises
