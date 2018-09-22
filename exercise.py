4123e70106de9f3ec7dd013f3139d1f35011a636
Wairton de Abreu <wairtonjr@gmail.com>
2018-09-22 11:58:50 +0200

    Building exercise module

--- exercise.py
+++ exercise.py
@@ -9,7 +9,7 @@ def get_random_note(lower=None, higher=None):
     Player().perform([lower + Semitones(n)])
 
 
-def build_ascending_scale_exercise(min_root=notes.A2, max_root=notes.GS5, intervals=[Semitones(1)]):
+def build_ascending_scale_exercise(min_root=notes.C2, max_root=notes.B5, intervals=[Semitones(1)]):
    min_reference = Reference.note_to_reference(min_root)
    max_reference = Reference.note_to_reference(max_root)
    exercises = []
@@ -19,3 +19,45 @@ def build_ascending_scale_exercise(min_root=notes.A2, max_root=notes.GS5, interv
        other_note = root_note + interval
        exercises.append((root_note, other_note, interval))
    return exercises
+
+
+class Exercise:
+    pass
+
+
+class HarmonicExercise(Exercise):
+    pass
+
+
+class MelodicExercise(Exercise):
+    pass
+
+
+def get_random_note(min_note=notes.C1, max_note=notes.B5):
+   min_reference = Reference.note_to_reference(min_note)
+   max_reference = Reference.note_to_reference(max_note)
+   return Reference.reference_to_note(random.randint(min_reference, max_reference))
+
+
+class IntervalIdentificationMelodicExercise(MelodicExercise):
+    def __init__(self, **config):
+        self.config = {
+            'min_root': config.get('min_root', notes.C2),
+            'max_root': config.get('max_root', notes.B5),
+            'intervals': config.get('intervals', [Semitones(1)]),
+            'repetition': config.get('repetition', 10)
+        }
+
+    def generate_exercises(self):
+        exercises = []
+        for i in range(self.config['repetition']):
+            root_note = get_random_note(self.config['min_root'], self.config['max_root'])
+            interval = random.choice(self.config['intervals'])
+            other_note = root_note + interval
+            exercises.append((root_note, other_note, interval))
+        return exercises
+
+
+class MelodicExerciseStep:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from music import Note
 from player import play
 
 from music import notes, Semitones, Reference
-from exercise import build_ascending_scale_exercise
+from exercise import IntervalIdentificationMelodicExercise
 
 
 intervals = [Semitones(i) for i in range(2, 9)]
-exs = build_ascending_scale_exercise(intervals=intervals)
+exercise = IntervalIdentificationMelodicExercise(intervals=intervals, repetition=5)
 
-for (a, b, c) in exs:
+
+for (a, b, c) in exercise.generate_exercises():
     print('-' * 50)
     print(a, b, c)
     play(a)
